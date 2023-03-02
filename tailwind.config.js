7d2f8fd3b2a0769985d256b28dced55447ba3fad
Faisal6274 <faisaldevbhaluka@gmail.com>
2023-03-02 17:49:34 +0600

    now create navbar design html and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#76d5ef",
+        "secondary": "#ee7271",
+        "black": "#052b3e",
+       
+    }
+    },
+  },
+  plugins: [],
+}
